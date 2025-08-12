7414b8e9d3390277e42e49aca1e20b8242788696
hesam-sa <hesam.sadat@gmail.com>
2025-08-12 23:57:13 +0330

    some changes
    to improve some style for better view

--- todo_project/todo_app/urls.py
+++ todo_project/todo_app/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('add/',views.add_task,name='add_task'),
     path('edit/<int:task_id>',views.edit_task,name='edit_task'),
     path('delete/<int:task_id>',views.delete_task,name='delete_task'),
+    path('register/',views.register_view,name='register')
 
 
 ]
--- todo_project/todo_app/views.py
+++ todo_project/todo_app/views.py
@@ -5,6 +5,9 @@ from django.contrib.auth.decorators import login_required
 from .models import Task
 from .forms import TaskForm
 from django.shortcuts import get_object_or_404
+from .forms import RegisterForm
+from django.contrib.auth import login
+
 
 @login_required
 def task_list(request):
@@ -71,3 +74,14 @@ def delete_task(request,task_id):
         return redirect('task_list')
     return render(request,"todo_app/delete_task.html",{'task': task})
 
+
+def register_view(requset):
+    if requset.method == 'POST' :
+        form = RegisterForm(requset.POST)
+        if form.is_valid():
+            user = form.save()
+            login(requset,user)
+            return redirect('task_list')
+    else:
+        form=RegisterForm()
+    return render(requset,'todo_app/register.html',{'form':form})
